4f221d51f88a636d23ea645582f0df5ad47016bf
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2023-01-16 21:22:01 +0100

    Map Eclipse JDT nodes to PSI nodes

--- src/org/refactoringminer/astDiff/matchers/Constants.java
+++ src/org/refactoringminer/astDiff/matchers/Constants.java
@@ -1,31 +1,35 @@
 package org.refactoringminer.astDiff.matchers;
 
-public class Constants {
-	// TODO replace with PSI labels
-	// AST node type labels
-	public static final String ASSIGNMENT = "Assignment";
-	public static final String METHOD_INVOCATION = "MethodInvocation";
-	public static final String METHOD_DECLARATION = "MethodDeclaration";
-	public static final String TRY_STATEMENT = "TryStatement";
-	public static final String CATCH_CLAUSE = "CatchClause";
-	public static final String BLOCK = "Block";
-	public static final String VARIABLE_DECLARATION_FRAGMENT = "VariableDeclarationFragment";
-	public static final String FIELD_DECLARATION = "FieldDeclaration";
-	public static final String ACCESS_MODIFIER = "AccessModifier";
-	public static final String PACKAGE_DECLARATION = "PackageDeclaration";
-	public static final String ANONYMOUS_CLASS_DECLARATION = "AnonymousClassDeclaration";
-	public static final String LABELED_STATEMENT = "LabeledStatement";
-	public static final String SIMPLE_NAME = "SimpleName";
-	public static final String VARIABLE_DECLARATION_STATEMENT = "VariableDeclarationStatement";
-	public static final String EXPRESSION_STATEMENT = "ExpressionStatement";
-	public static final String MODIFIER = "Modifier";
-	public static final String IMPORT_DECLARATION = "ImportDeclaration";
-	public static final String PRIMITIVE_TYPE = "PrimitiveType";
-	public static final String TYPE_DECLARATION = "TypeDeclaration";
-	public static final String ENUM_DECLARATION = "EnumDeclaration";
+import com.intellij.psi.JavaTokenType;
 
-	// AST node property labels
-	public static final String ASSIGNMENT_OPERATOR = "ASSIGNMENT_OPERATOR";
+public class Constants {
+	// PSI node type labels
+	public static final String ASSIGNMENT = "PsiAssignmentExpression";
+	public static final String METHOD_INVOCATION = "PsiMethodCallExpression";
+	public static final String METHOD_DECLARATION = "PsiMethod";
+	public static final String TRY_STATEMENT = "PsiTryStatement";
+	public static final String CATCH_CLAUSE = "PsiCatchSection";
+	public static final String BLOCK = "PsiCodeBlock";
+	public static final String VARIABLE_DECLARATION_FRAGMENT = "PsiLocalVariable";
+	public static final String FIELD_DECLARATION = "PsiField";
+	public static final String ACCESS_MODIFIER = "PsiModifier";
+	public static final String PACKAGE_DECLARATION = "PsiPackageStatement";
+	public static final String ANONYMOUS_CLASS_DECLARATION = "PsiAnonymousClass";
+	public static final String LABELED_STATEMENT = "PsiLabeledStatement";
+	public static final String SIMPLE_NAME = "PsiIdentifier";
+	public static final String VARIABLE_DECLARATION_STATEMENT = "PsiLocalVariable";
+	public static final String EXPRESSION_STATEMENT = "PsiExpression";
+	public static final String MODIFIER = "PsiModifier";
+	public static final String IMPORT_DECLARATION = "PsiImportList";
+	public static final String PRIMITIVE_TYPE = "PsiPrimitiveType";
+	public static final String TYPE_DECLARATION = "PsiClass";
+	public static final String ENUM_DECLARATION = "PsiEnumConstant";
+
+	// PSI node property labels
+	/**
+	 * 	See {@link JavaTokenType#EQ}.
+	 */
+	public static final String ASSIGNMENT_OPERATOR = "PsiJavaToken";
 	public static final String TYPE_DECLARATION_KIND = "TYPE_DECLARATION_KIND";
 
 	// Keyword labels
