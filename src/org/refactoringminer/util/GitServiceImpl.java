997e30c6c4e10c7349c1b3959a6f0d52f06ffc63
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2023-01-30 12:46:37 +0100

    Open Git repository using getRepositoryForRootQuick()

--- src/org/refactoringminer/util/GitServiceImpl.java
+++ src/org/refactoringminer/util/GitServiceImpl.java
@@ -1,14 +1,5 @@
 package org.refactoringminer.util;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vfs.LocalFileSystem;
@@ -20,11 +11,17 @@ import git4idea.commands.Git;
 import git4idea.commands.GitCommandResult;
 import git4idea.history.GitHistoryUtils;
 import git4idea.repo.GitRepository;
-import git4idea.repo.GitRepositoryImpl;
-
+import git4idea.repo.GitRepositoryManager;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.refactoringminer.api.GitService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GitServiceImpl implements GitService {
 
@@ -32,34 +29,37 @@ public class GitServiceImpl implements GitService {
 	Logger logger = LoggerFactory.getLogger(GitServiceImpl.class);
 
 	@Override
-	public GitRepository cloneIfNotExists(Project project, String projectPath, String cloneUrl/*, String branch*/) throws Exception {
-		java.io.File folder = new File(projectPath);
+	public GitRepository cloneIfNotExists(Project project, String projectPath, String cloneUrl) throws Exception {
+		File folder = new File(projectPath);
+
 		if (folder.exists()) {
-			VirtualFile rootDir = LocalFileSystem.getInstance().findFileByIoFile(folder);
-			return GitRepositoryImpl.getInstance(rootDir, project, false);
-			//logger.info("Project {} is already cloned, current branch is {}", cloneUrl, repository.getBranch());
+			logger.info("Project {} is already cloned.", cloneUrl);
+			return getRepository(project, folder);
 		} else {
-			String parentPath = projectPath.substring(0, projectPath.lastIndexOf("/"));
-			String projectName = projectPath.substring(projectPath.lastIndexOf("/") + 1, projectPath.length());
 			logger.info("Cloning {} ...", cloneUrl);
-			GitCommandResult result = Git.getInstance().clone(project, new File(parentPath), cloneUrl, projectName);
-			if(result.success()) {
-				VirtualFile rootDir = LocalFileSystem.getInstance().findFileByIoFile(folder);
-				return GitRepositoryImpl.getInstance(rootDir, project, false);
+			String projectName = folder.getName();
+			GitCommandResult result = Git.getInstance().clone(project, folder.getParentFile(), cloneUrl, projectName);
+			if (result.success()) {
+				return getRepository(project, folder);
 			}
 		}
 		return null;
 	}
 
+	@Nullable
+	private GitRepository getRepository(Project project, @NotNull File path) throws Exception {
+		VirtualFile rootDir = LocalFileSystem.getInstance().findFileByIoFile(path);
+		if (rootDir != null) {
+            return GitRepositoryManager.getInstance(project).getRepositoryForRootQuick(rootDir);
+        } else {
+            throw new FileNotFoundException(path.getAbsolutePath());
+		}
+	}
+
 	@Override
 	public GitRepository openRepository(Project project, String repositoryPath) throws Exception {
 		File folder = new File(repositoryPath);
-		if (folder.exists()) {
-			VirtualFile rootDir = LocalFileSystem.getInstance().findFileByIoFile(folder);
-			return GitRepositoryImpl.getInstance(rootDir, project, false);
-		} else {
-			throw new FileNotFoundException(repositoryPath);
-		}
+		return folder.exists() ? getRepository(project, folder) : null;
 	}
 
 	public List<? extends TimedVcsCommit> createAllRevsWalk(GitRepository repository) throws Exception {
@@ -101,31 +101,31 @@ public class GitServiceImpl implements GitService {
 	}
 
 	public void fileTreeDiff(GitRepository repository, Collection<Change> changes, Set<String> javaFilesBefore, Set<String> javaFilesCurrent, Map<String, String> renamedFilesHint) throws Exception {
-		for(Change change : changes) {
+		for (Change change : changes) {
 			Change.Type changeType = change.getType();
 			if (changeType != Change.Type.NEW) {
 				String oldRelativePath = VcsFileUtil.relativePath(repository.getRoot(), change.getBeforeRevision().getFile());
-				if (isJavafile(oldRelativePath)) {
+				if (isJavaFile(oldRelativePath)) {
 					javaFilesBefore.add(oldRelativePath);
 				}
 			}
 			if (changeType != Change.Type.DELETED) {
 				String newRelativePath = VcsFileUtil.relativePath(repository.getRoot(), change.getAfterRevision().getFile());
-				if (isJavafile(newRelativePath)) {
+				if (isJavaFile(newRelativePath)) {
 					javaFilesCurrent.add(newRelativePath);
 				}
 			}
 			if (change.isRenamed() || change.isMoved()) {
 				String oldRelativePath = VcsFileUtil.relativePath(repository.getRoot(), change.getBeforeRevision().getFile());
 				String newRelativePath = VcsFileUtil.relativePath(repository.getRoot(), change.getAfterRevision().getFile());
-				if (isJavafile(oldRelativePath) && isJavafile(newRelativePath)) {
+				if (isJavaFile(oldRelativePath) && isJavaFile(newRelativePath)) {
 					renamedFilesHint.put(oldRelativePath, newRelativePath);
 				}
 			}
 		}
 	}
 
-	private boolean isJavafile(String path) {
+	private boolean isJavaFile(String path) {
 		return path.endsWith(".java");
 	}
 }
